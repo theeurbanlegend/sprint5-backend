726169e5a03f2e4750d23839d52867a3e967c840
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 18:49:37 +0300

    more debugging

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ app.use(cors({
     origin: 'https://sprint5-front.vercel.app',
     credentials: true,
 }))
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 app.use(cookieParser())
 // ROUTES
 app.use('/img', express.static(path.join(__dirname,'img')));
