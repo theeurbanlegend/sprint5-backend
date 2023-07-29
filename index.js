f26cf5fd92c8e14a4faeda45f90e3365775f00c1
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 18:59:00 +0300

    more debugging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
     credentials: true,
 }))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
