d5f9eb97618f90d399497fcabc78338e926f8879
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 19:59:35 +0300

    more

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(logger)
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
