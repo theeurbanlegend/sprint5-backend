364bb54161a913c9907b458bbdc927584ec2e818
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 22:32:06 +0300

    update

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -5,9 +5,9 @@ const loginLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 5, // Limits each IP to 5 login requests per `window` per minute
     message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+        { msg: 'Too many login attempts from this IP, please try again after a 60 second pause' },
     handler: (req, res, next, options) => {
-        logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+        logEvents(`Too Many Requests: ${options.message.msg}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
         res.status(options.statusCode).send(options.message)
     },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
