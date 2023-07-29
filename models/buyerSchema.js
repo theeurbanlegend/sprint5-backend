f00781beff7a304b753719b3b2f14fccf4aef3ad
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 22:43:47 +0300

    update

--- models/buyerSchema.js
+++ models/buyerSchema.js
@@ -10,14 +10,20 @@ const buyerSchema=new mongoose.Schema({
         required:true
     },
     roles:[{
-        type:String,
-        default:["User"],
+        type:String
     }],
     cartItems:[{
         type:mongoose.Types.ObjectId,
         ref:'Item'
     }]
     
+    
 },{timestamps:true})
 
+buyerSchema.pre('save', function (next) {
+    if (this.isNew && this.roles.length === 0) {
+        this.roles.push("User");
+    }
+        next();
+})     
 module.exports=mongoose.model('Buyer',buyerSchema)
