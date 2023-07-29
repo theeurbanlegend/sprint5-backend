2ae103e3f3148026c5b7411ab0fa87bcdc19d73e
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-07-29 19:01:41 +0300

    further debugging

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const errHandler=require('./middleware/errhandler')
 const app=express()
 app.use(logger)
 app.use(express.json())
-app.use(cors({
-    origin: 'https://sprint5-front.vercel.app',
-    credentials: true,
-}))
+app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://sprint5-front.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
