917f54ec9261730e1e0dc326e6f766bd584afce9
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-08-20 22:01:30 +0300

    further Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
  const allowedOptions=[
+   'http://localhost:5173',
     'https://sprint5-front.vercel.app',
-    'https://front-l81f.onrender.com',
-    'https://localhost:5173'
+    'https://front-l81f.onrender.com'
+    
 
  ]
  module.exports=allowedOptions
