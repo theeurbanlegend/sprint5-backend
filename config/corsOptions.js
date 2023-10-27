d209d95833079dfe02ef9124236295ed4fc19c5c
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-10-27 20:19:02 +0300

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOptions=require('./allowedOrigins')
 
 const corsOptions={
     origin:(origin,callback)=>{
-        if(allowedOptions.indexOf(origin)!==-1||!origin||origin.endsWith('/socket.io/')){
+        if(allowedOptions.indexOf(origin)!==-1||!origin){
             callback(null,true)
         }else{
             callback(new Error('Not allowed by CORS'))
