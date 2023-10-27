a2a8e88b03af97c0e72af2473a37e625929f7f9f
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-10-27 21:38:56 +0300

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOptions=require('./allowedOrigins')
 
 const corsOptions = {
-  origin: 'https://sprint5-front.vercel.app', // Replace with the actual URL of your specific route
+  origin: 'https://front-l81f.onrender.com', // Replace with the actual URL of your specific route
   credentials: true
 }
 module.exports=corsOptions
